c947032392e697ee170a88f0c2e5a8ab5623af53
Dwain Maralack <dwain.maralack@gmail.com>
2014-11-09 21:06:20 -0800

    created the /sensei route and enpoint that responds with the basic plugin information

--- classes/class-woothemes-sensei-rest-api.php
+++ classes/class-woothemes-sensei-rest-api.php
@@ -16,41 +16,61 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  * - __construct()
  *
- * TODO: check for WP-API
  *
  */
 class WooThemes_Sensei_Rest_API extends WP_JSON_CustomPostType {
 
     public function __construct () {
+
         //register the endpoints
-        add_filter( 'json_endpoints', array( $this , 'register_routes' ) );
+        add_filter( 'json_endpoints', array( $this , 'register_sensei_routes' ) );
+
     }// __construct
 
-    public function register_routes( $routes ){
-            $routes['/sensei/'] = array(
+	/**
+	 * Register the routes to enable Sensei to respond to jason API requests
+	 *
+	 * @since 1.7.0
+	 * @param array $routes
+	 * @return array $routes
+	 */
+    public function register_sensei_routes ( $routes ){
+
+	        // The main sensei rout that provides plugin information and sub routes
+            $routes['/sensei'] = array(
                 array( array( $this, 'send_plugin_data'), WP_JSON_Server::READABLE ),
-                //array( array( $this, 'new_post'), WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
             );
-            $routes['/sensei/(?P<username>\w+)/courses'] = array(
-                array( array( $this, 'get_user_courses'), WP_JSON_Server::READABLE ),
-                //array( array( $this, 'new_post'), WP_JSON_Server::CREATABLE | WP_JSON_Server::ACCEPT_JSON ),
-            );
-            $routes['/sensei/courses/(?P<id>\d+)'] = array(
-                array( array( $this, 'get_post'), WP_JSON_Server::READABLE ),
-               // array( array( $this, 'edit_post'), WP_JSON_Server::EDITABLE | WP_JSON_Server::ACCEPT_JSON ),
-               // array( array( $this, 'delete_post'), WP_JSON_Server::DELETABLE ),
-            );
+
 
             // Add more custom routes here
             return $routes;
-    }// end register_routes
 
-    public function send_plugin_data( ) {
-        $test = array( 'plugin'=>'Sensei', 'version' => 'API Beta' , 'success' => true );
-        wp_send_json_success( $test );
+    }// end register_sensei_routes
+
+	/**
+	 * Return Sensei specific plugin data for the /sensei route
+	 *
+	 * @since 1.7.0
+	 */
+    public function send_plugin_data ( ) {
+
+	    global $woothemes_sensei;
+        $sensei_data = array( 'plugin'=>'Sensei', 'version' => $woothemes_sensei->version );
+
+        // call the wordpress function that returns the data with a success header
+	    wp_send_json_success( $sensei_data );
+
     }// end send plugin data
 
-    public function get_user_courses () {
-        //
-    }// end get_user_courses
+	public function get_user_courses () {
+		//
+	}// end get_user_courses
+
+$routes['/sensei/(?P<username>\w+)/courses'] = array(
+array( array( $this, 'get_user_courses'), WP_JSON_Server::READABLE ),
+);
+$routes['/sensei/courses/(?P<id>\d+)'] = array(
+array( array( $this, 'get_post'), WP_JSON_Server::READABLE ),
+);
+
 }// end class
